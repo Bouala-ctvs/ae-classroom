5f4a3a72c64516546a5ef502f42637ee67c76bb1
Bouala-ctvs <135937259+Bouala-ctvs@users.noreply.github.com>
2023-09-12 13:06:18 +0700

    Update mongodb.js

--- api/database/mongodb.js
+++ api/database/mongodb.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-
+mongoose.set('strictQuery', true);
+             
 async function connectDB(params) {
   const uri =
     "mongodb+srv://boualactvs:Lair1996@cluster0.rbogwkz.mongodb.net/ae_classroom?retryWrites=true&w=majority";
