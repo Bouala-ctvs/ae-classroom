60e13ad9809b8291b31606c2676bd57f98d33689
Bouala-ctvs <135937259+Bouala-ctvs@users.noreply.github.com>
2023-09-12 21:52:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,19 @@ var fs = require("fs");
 // app.use(bodyParser.urlencoded({
 //   extended: false
 // }));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({
+  limit: '50mb'
+}));
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
 
 // parse application/json
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/files',express.static('files'))
+app.use('/files', express.static('files'))
 
 
 app.use((req, res, next) => {
@@ -45,14 +50,12 @@ app.use("/api/game/testing", GameTestingRouter);
 
 // Find 404 and hand over to error handler
 app.use((req, res, next) => {
-  return next(createError(404, 'File not found'));
+  return next(res.status(404).send("404 Not Fund"));
 });
-
-
 
 // error handler
 app.use(function (err, req, res, next) {
-  console.error(err.message); // Log error message in our server's console
+  // console.error(err.message); // Log error message in our server's console
   if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
   res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
 });
